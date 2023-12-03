aa98b9ae096999a949d32012725afffc8ebfd3ed
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-12-04 00:47:23 +0530

    feat: google github sign in

---
+++ netflix-clone/app/components/GoogleSignInButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import GoogleIcon from "../../../public/google-icon.png"
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+
+export default function GithubSignInButton() {
+    return (
+        <Button onClick={() => signIn("github")} variant='outline' size='icon'>                    
+        <Image 
+            src={GoogleIcon} 
+            alt="google icon" 
+            className="w-4 h-4"
+        />
+        </Button>
+    );
+}

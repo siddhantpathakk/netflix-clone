d22a9d3d7a3b3538041cbfba91b809e84deb17f3
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-12-06 14:43:04 +0530

    edit: fix navbar layout issue

--- netflix-clone/app/home/layout.tsx
+++ netflix-clone/app/home/layout.tsx
@@ -1,16 +1,26 @@
+import Navbar from "../components/Navbar";
 import { ReactNode } from "react";
 import { authOptions } from "../utils/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-export default async function HomeLayout({children} : {children: ReactNode}) {
-    const session = await getServerSession(authOptions);
-    if(!session){
-        return redirect('/login');
-    }
-    return(
-        <div>
-            {children}
-        </div>
-    )
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/login");
+  }
+
+  return (
+    <>
+      <Navbar />
+      <main className="w-full max-w-7xl mx-auto sm:px-6 lg:px-8">
+        {children}
+      </main>
+    </>
+  );
 }

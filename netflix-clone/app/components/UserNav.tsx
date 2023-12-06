f78c98cdaaa43f7c7394df80116a2cacae36cc4d
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-12-06 14:42:38 +0530

    edit: fix navbar layout issue

--- netflix-clone/app/components/UserNav.tsx
+++ netflix-clone/app/components/UserNav.tsx
@@ -1,33 +1,42 @@
 "use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 
 export default function UserNav() {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant='ghost' className="relative h-10 w-10 rounded-sm">
-                    <Avatar className='h-10 w-10 rounded-sm'>
-                        <AvatarImage src='https://uknqszeudspmcjwpgthh.supabase.co/storage/v1/object/public/user%20image/default.png'/>
-                        <AvatarFallback className="rounded-sm">User</AvatarFallback>
-                    </Avatar>
-                </Button>
-            </DropdownMenuTrigger>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-sm">
+          <Avatar className="h-10 w-10 rounded-sm">
+            <AvatarImage src="https://uknqszeudspmcjwpgthh.supabase.co/storage/v1/object/public/user%20image/default.png" />
+            <AvatarFallback className="rounded-sm">User</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
 
-            <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel>
-                    <div className ='flex flex-col space-y-1'>
-                        <p className="text-sm font-medium leading-xs">Siddhant</p>
-                        <p className="text-xs text-muted-foreground leading-xs">siddhant005 </p>
-                    </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => signOut()}>Sign Out.</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel>
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">John</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              example@mail.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
